a45326a1ff08cbe8d2bd2ad7eccba2dfd17d9391
Nathan Borror <nathan@nathanborror.com>
2018-01-10 15:53:25 -0800

    Fixing apns2 command so it accepts and auth-token. Also removing unnecessary kingpin dependency.

--- apns2/main.go
+++ apns2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,29 +10,59 @@ import (
 
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/sideshow/apns2/token"
 )
 
 var (
-	certificatePath = kingpin.Flag("certificate-path", "Path to certificate file.").Required().Short('c').String()
-	topic           = kingpin.Flag("topic", "The topic of the remote notification, which is typically the bundle ID for your app").Required().Short('t').String()
-	mode            = kingpin.Flag("mode", "APNS server to send notifications to. `production` or `development`. Defaults to `production`").Default("production").Short('m').String()
+	certificatePath = flag.String("certificate-path", "", "Path to certificate file.")
+	authTokenPath   = flag.String("auth-token-path", "", "Path to auth token file.")
+	authTokenKeyID  = flag.String("auth-token-key", "", "Auth token Key ID (required with auth-token-path)")
+	authTokenTeamID = flag.String("auth-token-team", "", "Auth token Team ID (required with auth-token-path)")
+	topic           = flag.String("topic", "", "The topic of the remote notification, which is typically the bundle ID for your app")
+	mode            = flag.String("mode", "production", "APNS server to send notifications to. `production` or `development`.")
 )
 
+func usage() {
+	fmt.Fprint(os.Stderr, `Listens to STDIN to send notifications and writes APNS response code and reason to STDOUT.
+Certificate or AuthToken are required. The expected format is: <DeviceToken> <APNS Payload>
+Example: aff0c63d9eaa63ad161bafee732d5bc2c31f66d552054718ff19ce314371e5d0 {"aps": {"alert": "hi"}}
+
+`)
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
 func main() {
-	kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version("0.1").Author("Alisson Sales")
-	kingpin.CommandLine.Help = `Listens to STDIN to send notifications and writes APNS response code and reason to STDOUT.
-	The expected format is: <DeviceToken> <APNS Payload>
-	Example: aff0c63d9eaa63ad161bafee732d5bc2c31f66d552054718ff19ce314371e5d0 {"aps": {"alert": "hi"}}`
-	kingpin.Parse()
+	flag.Usage = usage
+	flag.Parse()
 
-	cert, pemErr := certificate.FromPemFile(*certificatePath, "")
+	var client *apns2.Client
 
-	if pemErr != nil {
-		log.Fatalf("Error retrieving certificate `%v`: %v", certificatePath, pemErr)
+	if *authTokenPath != "" {
+		if *authTokenKeyID == "" || *authTokenTeamID == "" {
+			log.Fatalf("Must specify token-key and token-team when using token based auth")
+		}
+		authKey, err := token.AuthKeyFromFile(*authTokenPath)
+		if err != nil {
+			log.Fatalf("Error retrieving auth-token `%v`: %v", *authTokenPath, err)
+		}
+		token := &token.Token{
+			AuthKey: authKey,
+			KeyID:   *authTokenKeyID,
+			TeamID:  *authTokenTeamID,
+		}
+		client = apns2.NewTokenClient(token)
 	}
-
-	client := apns2.NewClient(cert)
+	if *certificatePath != "" {
+		cert, err := certificate.FromPemFile(*certificatePath, "")
+		if err != nil {
+			log.Fatalf("Error retrieving certificate `%v`: %v", *certificatePath, err)
+		}
+		client = apns2.NewClient(cert)
+	}
+	if client == nil {
+		usage()
+	}
 
 	if *mode == "development" {
 		client.Development()
